1cb4ba5fa457847170db13184c31ce82e87e7a12
acoshiba www <ant4rctica@gmail.com>
2023-12-12 07:57:07 +0000

    creating images

--- task.py
+++ task.py
@@ -1,9 +1,1 @@
-const http = require("node:http");
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({message: "Hello, docker"}));
-});
-
-server.listen(5050, () => console.log('Http server started!'));
+print("Hello, world");
